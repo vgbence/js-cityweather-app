d88dadd4f36e11e260827a9519f304244b9ec5f5
vgbence <vgbence@yahoo.com>
2022-03-24 07:28:16 +0100

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+const key = "tYnJ40POeLZQcnYOR1eRzujglPHDJpDJ"
+
+const getWeather = async (id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query)
+    const data = await response.json();
+    return data[0]
+}
+
+const getCity = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0]
+}
+
+// getCity("budapest").then(data => {
+//     return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data)
+// }).catch(err => console.log(err))
+
