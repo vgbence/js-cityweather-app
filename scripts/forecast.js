79d0df744d71f57e97e8f4fa85f6e7f5b751b856
vgbence <vgbence@yahoo.com>
2022-03-24 07:41:56 +0100

    netlify error fix, https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,7 @@
 const key = "7eU4DgQZnJmnnpfJ5EXGly1ByHOx4BSe"
 
 const getWeather = async (id) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
     const response = await fetch(base + query)
     const data = await response.json();
@@ -9,16 +9,9 @@ const getWeather = async (id) => {
 }
 
 const getCity = async (city) => {
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`
     const response = await fetch(base + query);
     const data = await response.json();
     return data[0]
 }
-
-// getCity("budapest").then(data => {
-//     return getWeather(data.Key)
-// }).then(data => {
-//     console.log(data)
-// }).catch(err => console.log(err))
-
